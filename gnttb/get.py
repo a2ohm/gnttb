7860b6d930f4f2d6c4f57558e26b28084f3c43db
Antoine <antoine@2ohm.fr>
2023-03-09 22:18:21 +0100

    Accept to get a verse from a biblical reference

--- gnttb/get.py
+++ gnttb/get.py
@@ -3,15 +3,22 @@
 
 from .sblgnt import morphgnt_rows
 from .verse  import Verse
-from .bcv import checkBcv, expandBcv, splitBcv
+from .bcv import str2bcv, checkBcv, expandBcv, splitBcv
 
-def get(bcv):
-    """Return a group of verses given their bcv.
+def get(ref):
+    """Return a group of verses given a biblical reference
+
+    The biblical reference can be litteral (eg. Ro 1, 12) or a bcv.
 
     bcv syntax:
         bbccvv          --> one verse
         bbccvv-bbccvv   --> group of verses
     """
+
+    if "0" < ref[1] < "9":
+        bcv = ref.strip()
+    else:
+        bcv = str2bcv(ref)
 
     if checkBcv(bcv):
         bcv_start, bcv_end = splitBcv(bcv)
